faf7b4b648c0819021553b848a41170fb77d07f0
felipenicoletti <felipe.nico@gmail.com>
2026-06-08 12:04:39 -0300

    visitas.js

---
+++ visitas.js
@@ -0,0 +1,34 @@
+const visitas = [
+  {
+    "data_visita": "2026-06-02",
+    "hora_visita": "10:00"
+  },
+  {
+    "data_visita": "2026-06-04",
+    "hora_visita": "10:00"
+  },
+  {
+    "data_visita": "2026-06-04",
+    "hora_visita": "10:00"
+  },
+  {
+    "data_visita": "2026-06-09",
+    "hora_visita": "10:00"
+  },
+  {
+    "data_visita": "2026-06-02",
+    "hora_visita": "11:00"
+  },
+  {
+    "data_visita": "2026-06-02",
+    "hora_visita": "12:00"
+  },
+  {
+    "data_visita": "2026-06-02",
+    "hora_visita": "12:00"
+  },
+  {
+    "data_visita": "2026-06-03",
+    "hora_visita": "10:00"
+  }
+];
