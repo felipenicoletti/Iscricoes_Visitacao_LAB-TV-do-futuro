9a4df600ef0149ba808dfb9be6a7e240951f107a
felipenicoletti <felipe.nico@gmail.com>
2026-06-24 10:21:42 -0300

    chore: atualiza visitas.js

--- visitas.js
+++ visitas.js
@@ -14,5 +14,13 @@ const visitas = [
   {
     "data_visita": "2026-07-08",
     "hora_visita": "12:00"
+  },
+  {
+    "data_visita": "2026-07-15",
+    "hora_visita": "10:00"
+  },
+  {
+    "data_visita": "2026-07-15",
+    "hora_visita": "12:00"
   }
 ];
