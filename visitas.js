9f607a5e71b377786d1dc18968f3d8dba81e3a26
felipenicoletti <felipe.nico@gmail.com>
2026-08-27 10:09:07 -0300

    Atualiza visitas.js via Apps Script - 2026-08-27T13:09:07.309Z

--- visitas.js
+++ visitas.js
@@ -295,6 +295,11 @@ const visitas = [
     "qtd_visitas": 6
   },
   {
+    "data_visita": "2026-09-01",
+    "hora_visita": "12:00",
+    "qtd_visitas": 1
+  },
+  {
     "data_visita": "2026-09-02",
     "hora_visita": "10:00",
     "qtd_visitas": 3
